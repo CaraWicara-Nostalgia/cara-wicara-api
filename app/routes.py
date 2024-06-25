53afbbcf7877813b762b40b2d47a43c72f75545f
Ilham Yudantyo <ilhamyudantyo@gmail.com>
2024-06-25 13:37:19 +0700

    feat: add blok trycatch

--- app/routes.py
+++ app/routes.py
@@ -7,9 +7,14 @@ def root():
 
 @app.route('/index', methods=['get'])
 def index():
-    data = {
-        "message":"success",
-        "status":200,
-        "data": {}
-    }
-    return jsonify(data), 400
+    try:
+        data = {
+            "message":"success",
+            "status":200,
+            "data": {}
+        }
+        asd
+        return jsonify(data), 400
+    except Exception as e: 
+        print('error: ', e)
+        return jsonify({"message": "Internal Server Error", "status": 500,}), 500
